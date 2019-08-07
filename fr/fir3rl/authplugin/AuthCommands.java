8040b1d1f90eb054cea2a214e7333a815420ecab
DeadCube <49811163+DeadCube@users.noreply.github.com>
2019-08-08 00:46:01 +0200

    Update AuthCommands.java

--- fr/fir3rl/authplugin/AuthCommands.java
+++ fr/fir3rl/authplugin/AuthCommands.java
@@ -20,45 +20,69 @@ public class AuthCommands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
 		// TODO Auto-generated method stub
-		if (sender instanceof Player) {
+
+		if (cmd.getName().equalsIgnoreCase("login") && sender instanceof Player) {
 
 			Player player = (Player) sender;
 
-			if (cmd.getName().equalsIgnoreCase("login")) {
-
-				if (args.length == 0) {
-					player.kickPlayer(main.readString("commands.nopass"));
-				}
-				if (main.newPlayers.contains(player.getName()))
-					;
-				String password = DigestUtils.sha256Hex(args[0]);
-				String json;
-				try {
-					json = JsonUtils.readUrl(main.readString("url") + "/auth/getDataLauncher?username="+ player.getName() + "&password=" + password);
-					// TODO Auto-generated catch block
-					if (json.startsWith("{")) {
-						main.newPlayers.remove(player.getName());
-						player.sendMessage(main.readString("commands.connected"));
-						for (String elem : main.readStrings("joinmessage")) {
-							Bukkit.broadcastMessage(elem.replaceAll("&", "§").replaceAll("%PLAYER%", player.getName()).replaceAll("%SERVER%", main.getServer().getName().replaceAll("%PLAYERS%", "" + this.main.getServer().getOnlinePlayers().length).replaceAll("%MAXPLAYERS%", String.valueOf(this.main.getServer().getMaxPlayers()))));
-						}
-					} else if (json.toString().contains("error_password")) {
-						player.kickPlayer(main.readString("commands.wrongpass"));
-					} else {
-						player.kickPlayer("Erreur inconnue");
+			if (args.length == 0) {
+				player.kickPlayer(main.readString("commands.login.nopass"));
+			}
+			if (main.newPlayers.contains(player.getName())) {
+				player.sendMessage(main.readString("commands.login.alreadyconnected"));
+			}
+			;
+			String password = DigestUtils.sha256Hex(args[0]);
+			String json;
+			try {
+				json = JsonUtils.readUrl(main.readString("url") + "/auth/getDataLauncher?username=" + player.getName()
+						+ "&password=" + password);
+				// TODO Auto-generated catch block
+				if (json.startsWith("{")) {
+					main.newPlayers.remove(player.getName());
+					player.sendMessage(main.readString("commands.login.connected"));
+					for (String elem : main.readStrings("joinmessage")) {
+						Bukkit.broadcastMessage(
+								elem.replaceAll("&", "§").replaceAll("%PLAYER%", player.getName())
+										.replaceAll("%SERVER%", main.getServer().getName()
+												.replaceAll("%PLAYERS%",
+														String.valueOf(this.main.getServer().getOnlinePlayers().length))
+												.replaceAll("%MAXPLAYERS%",
+														String.valueOf(this.main.getServer().getMaxPlayers()))));
 					}
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					player.kickPlayer("Erreur interne");
-					e.printStackTrace();
+				} else if (json.toString().contains("error_password")) {
+					player.kickPlayer(main.readString("commands.login.wrongpass"));
+				} else {
+					player.kickPlayer("Erreur inconnue");
 				}
-
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				player.kickPlayer("Erreur interne");
+				e.printStackTrace();
 			}
 
-		} else {
+		}
+
+		if (cmd.getName().equalsIgnoreCase("forcelogin")) {
+
+			if (args.length != 0) {
+
+				for (Player pl : main.getServer().getOnlinePlayers()) {
+					if (pl.getName().equalsIgnoreCase(args[0])) {
+						main.newPlayers.remove(args[0]);
+						pl.sendMessage(main.readString("commands.forcelogin.success"));
+						return false;
+					}
+				}
+				sender.sendMessage(main.readString("commands.forcelogin.noplayer"));
+
+			} else {
+				sender.sendMessage(main.readString("commands.forcelogin.needarg"));
+			}
 
 		}
 		return false;
+
 	}
 
 }
