24ce4c0af630c4bd7eacd200bd5b9e462d31240d
DeadCube <49811163+DeadCube@users.noreply.github.com>
2019-12-14 18:04:51 +0100

    Update AuthPlugin.java

--- fr/fir3rl/authplugin/AuthPlugin.java
+++ fr/fir3rl/authplugin/AuthPlugin.java
@@ -3,19 +3,22 @@ package fr.fir3rl.authplugin;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuthPlugin extends JavaPlugin {
 	public ArrayList<String> newPlayers = new ArrayList<String>();
+	public ArrayList<Location> newPlayersLocations = new ArrayList<>();
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
 		saveDefaultConfig();
-		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(new AuthEvents(this), this);
+		getServer().getPluginManager().registerEvents(new AuthEvents(this), this);
 		getCommand("login").setExecutor(new AuthCommands(this));
+		getCommand("forcelogin").setExecutor(new AuthCommands(this));
+		getCommand("unlock").setExecutor(new AuthCommands(this));
+		getCommand("authversion").setExecutor(new AuthCommands(this));
 	}
 
 	@Override
